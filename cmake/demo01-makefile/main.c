161d6bc2a0941b2380f9a726fc0e60fbe87db70e
XiCuod <xicuod@outlook.com>
2025-10-27 08:46:38 +0800

    初识cmake

---
+++ cmake/demo01-makefile/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "message.h"
+
+int main()
+{
+    printf("hello make, makefile and cmake!\n");
+    message();
+    return 0;
+}
---
+++ cmake/demo01-makefile/message.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void message() {
+    printf("hello this is a message\n");
+}
